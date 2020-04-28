bd7699542fe1f1fa4e10c917ad9a4a36045639d1
ananthprasad91 <ananthprasad91@gmail.com>
2020-04-28 22:00:29 +0530

    Updated Deployed Config URLs

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 const dev = {
     urls: {
-        USER_SERVICE: "http://localhost:5000/api/userService/",
-        EXPENSE_SERVICE: "http://localhost:4000/api/expService/",
-        AUTH_SERVICE: "http://localhost:7000/api/authService/"
+        USER_SERVICE: "http://34.87.166.85:5000/api/userService/",
+        EXPENSE_SERVICE: "http://34.87.53.224:4000/api/expService/",
+        AUTH_SERVICE: "http://34.87.100.137:7000/api/authService/"
     }
 }
 
 const prod = {
     urls: {
-        USER_SERVICE: "http://localhost:5000/api/userService/",
-        EXPENSE_SERVICE: "http://localhost:4000/api/expService/",
-        AUTH_SERVICE: "http://localhost:7000/api/authService/"
+        USER_SERVICE: "http://34.87.53.224:5000/api/userService/",
+        EXPENSE_SERVICE: "http://34.87.166.85:4000/api/expService/",
+        AUTH_SERVICE: "http://34.87.100.137:7000/api/authService/"
     }
 }
 
