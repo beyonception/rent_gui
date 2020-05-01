2bb404b892b6cca7d45b6f83e16a84e98876fa9b
ananthprasad91 <ananthprasad91@gmail.com>
2020-05-01 20:22:24 +0530

    Updated Config Files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,11 @@
+const local = {
+    urls: {
+        USER_SERVICE: "http://localhost:5000/api/userService/",
+        EXPENSE_SERVICE: "http://localhost:4000/api/expService/",
+        AUTH_SERVICE: "http://localhost:7000/api/authService/"
+    }
+}
+
 const dev = {
     urls: {
         USER_SERVICE: "http://34.87.166.85:5000/api/userService/",
@@ -14,9 +22,24 @@ const prod = {
     }
 }
 
-const config = process.env.REACT_APP_STAGE === 'prod'
-    ? prod
-    : dev;
+const getConfig = () => {
+    switch (process.env.REACT_APP_STAGE) {
+        case local: {
+            return local
+        }
+        case dev: {
+            return dev
+        }
+        case prod: {
+            return prod
+        }
+        default: {
+            return dev
+        }
+    }
+}
+
+const config = getConfig()
 
 export default {
     ...config
